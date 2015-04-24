d28165973e2e7f7fd0575c06c659da4690a74e7e
Lingliang Zhang <lingliangz@gmail.com>
2015-04-24 10:23:48 -0400

    Made containers namespace specific

--- persistent-start.sh
+++ persistent-start.sh
@@ -13,4 +13,4 @@ THEME_NAME="$(basename $1)"
 echo "Creating persistent container for theme name: $THEME_NAME"
 echo "PORT: $PORT"
 
-docker run -i -t -p 3000:$PORT --name $THEME_NAME -v $1:/usr/share/nginx/www/wp-content/themes/$THEME_NAME docker-wordpress-nginx
+docker run -i -t -p 80:$PORT --name $THEME_NAME -v $1:/usr/share/nginx/www/wp-content/themes/$THEME_NAME docker-wordpress-nginx
