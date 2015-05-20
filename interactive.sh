f805580f47dc3a50e17729bb5e8e02c109faf9d3
Lingliang Zhang <lingliangz@gmail.com>
2015-05-20 03:31:58 -0400

    swapped some backwards port entries

--- interactive.sh
+++ interactive.sh
@@ -13,4 +13,4 @@ THEME_NAME="$(basename $1)"
 echo "Creating interactive container for theme name: $THEME_NAME"
 echo "PORT: $PORT"
 
-docker run -i -t --rm -p 80:$PORT --name $THEME_NAME -v $1:/usr/share/nginx/www/wp-content/themes/$THEME_NAME docker-wordpress-nginx bash
+docker run -i -t --rm -p $PORT:80 --name $THEME_NAME -v $1:/usr/share/nginx/www/wp-content/themes/$THEME_NAME docker-wordpress-nginx bash
